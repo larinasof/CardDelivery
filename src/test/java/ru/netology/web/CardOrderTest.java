115cc6f822f3a9a73a6856209a7ff0d0571e5f6b
Sofia Larina <zazaq97@gmail.com>
2020-09-10 16:13:00 +0300

    checking of the date of meeting added

--- src/test/java/ru/netology/web/CardOrderTest.java
+++ src/test/java/ru/netology/web/CardOrderTest.java
@@ -1,5 +1,6 @@
 package ru.netology.web;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
@@ -34,7 +35,8 @@ public class CardOrderTest {
         form.$("[data-test-id='phone'] input").setValue("+79991234567");
         form.$("[data-test-id='agreement']").click();
         $$("button").find(exactText("Забронировать")).click();
-        $(withText("Успешно")).waitUntil(visible, 15000);
+//        $(withText("Успешно" + dateOfMeeting)).waitUntil(visible, 15000);
+        $("[data-test-id='notification'] ").waitUntil(visible, 15000).shouldHave(exactText("Успешно! Встреча успешно забронирована на " + dateOfMeeting));
     }
 
     @Test
