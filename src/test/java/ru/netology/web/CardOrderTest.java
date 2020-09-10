8c41cfdfb6308ca6203259ca2319628fb4eb964f
Sofia Larina <zazaq97@gmail.com>
2020-09-10 15:29:42 +0300

    date added

--- src/test/java/ru/netology/web/CardOrderTest.java
+++ src/test/java/ru/netology/web/CardOrderTest.java
@@ -2,6 +2,11 @@ package ru.netology.web;
 
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Calendar;
 
 import static com.codeborne.selenide.Selenide.open;
 
@@ -16,6 +21,16 @@ public class CardOrderTest {
         SelenideElement form = $("#root");
         form.$("[data-test-id='city'] input").setValue("Санкт-Петербург");
         form.$("[data-test-id='name'] input").setValue("Иванов Иван");
+        SelenideElement date = $("[data-test-id='date'] input");
+        date.doubleClick().sendKeys(Keys.BACK_SPACE);
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_MONTH, 3);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        String dateOfMeeting = dateFormat.format(calendar.getTime());
+//        LocalDate minDate = LocalDate.now();
+//        minDate = minDate.plusDays(3);
+//        String dateOfMeeting = dateFormat.format(minDate);
+        date.setValue(dateOfMeeting);
         form.$("[data-test-id='phone'] input").setValue("+79991234567");
         form.$("[data-test-id='agreement']").click();
         $$("button").find(exactText("Забронировать")).click();
@@ -28,6 +43,13 @@ public class CardOrderTest {
         SelenideElement form = $("#root");
         form.$("[data-test-id='city'] input").setValue("Череповец");
         form.$("[data-test-id='name'] input").setValue("Иванов Иван");
+        SelenideElement date = $("[data-test-id='date'] input");
+        date.doubleClick().sendKeys(Keys.BACK_SPACE);
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_MONTH, 3);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        String dateOfMeeting = dateFormat.format(calendar.getTime());
+        date.setValue(dateOfMeeting);
         form.$("[data-test-id='phone'] input").setValue("+79991234567");
         form.$("[data-test-id='agreement']").click();
         $$("button").find(exactText("Забронировать")).click();
